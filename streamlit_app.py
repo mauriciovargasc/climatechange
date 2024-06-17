a8b92b672ee64ff807a7e32abb828b83fe11a53c
mauriciovargasc <mauriciovargascolon@gmail.com>
2024-06-17 02:48:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,6 @@ def load_data():
 
 data, geojson_data = load_data()
 
-# Normalize country names
-iso3_to_country = {feature['properties']['ISO_A3']: feature['properties']['ADMIN'] for feature in geojson_data['features']}
-data['Country'] = data['ISO3'].map(iso3_to_country)
 
 # Streamlit Layout
 st.title("Climate Change Dashboard")
@@ -92,7 +89,7 @@ fig2 = px.bar(top_countries,
               y='Country', 
               orientation='h', 
               color='Temp_Increase',
-              color_continuous_scale=px.colors.sequential.Reds,
+              color_continuous_scale=tomato_colors,
               title=f'Top {n} Countries with Highest Temperature Increase (1961-2022)')
 fig2.update_layout(
     xaxis_title='Temperature Increase (°C)',
@@ -103,8 +100,41 @@ st.plotly_chart(fig2)
 
 # Section 4: Temperature Trends in Hemispheres
 st.header("Temperature Trends in Northern vs Southern Hemisphere")
-northern_hemisphere = ['USA', 'Canada', 'Russia', 'UK', 'Germany', 'China', 'France', 'Japan', 'Italy', 'India']
-southern_hemisphere = ['Australia', 'Brazil', 'South Africa', 'Argentina', 'Chile', 'New Zealand', 'Madagascar', 'Indonesia', 'Peru']
+northern_hemisphere = [
+    'Afghanistan, Islamic Rep. of', 'Albania', 'Algeria', 'Andorra, Principality of', 'Angola', 'Armenia, Rep. of',
+    'Austria', 'Azerbaijan, Rep. of', 'Bahamas, The', 'Bahrain, Kingdom of', 'Bangladesh', 'Belarus, Rep. of',
+    'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brunei Darussalam',
+    'Bulgaria', 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cambodia', 'Cameroon', 'Canada', 'Cayman Islands',
+    'Central African Rep.', 'Chad', 'China, P.R.: Hong Kong', 'China, P.R.: Macao', 'China, P.R.: Mainland', 'Colombia',
+    'Comoros, Union of the', 'Congo, Dem. Rep. of the', 'Congo, Rep. of', 'Costa Rica', 'Croatia, Rep. of', 'Cuba',
+    'Cyprus', 'Czech Rep.', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Rep.', 'Ecuador', 'Egypt, Arab Rep. of',
+    'El Salvador', 'Equatorial Guinea, Rep. of', 'Eritrea, The State of', 'Estonia, Rep. of', 'Eswatini, Kingdom of',
+    'Ethiopia, The Federal Dem. Rep. of', 'Finland', 'France', 'Gabon', 'Gambia, The', 'Georgia', 'Germany', 'Ghana',
+    'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana',
+    'Haiti', 'Holy See', 'Honduras', 'Hungary', 'Iceland', 'India', 'Iran, Islamic Rep. of', 'Iraq', 'Ireland',
+    'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan, Rep. of', 'Kuwait', 'Kyrgyz Rep.',
+    'Lao People\'s Dem. Rep.', 'Latvia', 'Lebanon', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg',
+    'Malaysia', 'Maldives', 'Malta', 'Mauritania, Islamic Rep. of', 'Mauritius', 'Mexico', 'Moldova, Rep. of', 'Monaco',
+    'Mongolia', 'Montenegro', 'Morocco', 'Myanmar', 'Nepal', 'Netherlands, The', 'Nicaragua', 'Niger', 'Nigeria',
+    'North Macedonia, Republic of', 'Norway', 'Oman', 'Pakistan', 'Panama', 'Philippines', 'Poland, Rep. of', 'Portugal',
+    'Puerto Rico', 'Qatar', 'Romania', 'Russian Federation', 'Rwanda', 'San Marino, Rep. of', 'Saudi Arabia', 'Senegal',
+    'Serbia, Rep. of', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovak Rep.', 'Slovenia, Rep. of', 'Somalia', 'Spain',
+    'Sri Lanka', 'St. Kitts and Nevis', 'St. Lucia', 'St. Vincent and the Grenadines', 'Sudan', 'Suriname', 'Sweden',
+    'Switzerland', 'Syrian Arab Rep.', 'Taiwan Province of China', 'Tajikistan, Rep. of', 'Thailand', 'Timor-Leste, Dem. Rep. of',
+    'Togo', 'Trinidad and Tobago', 'Tunisia', 'Turkmenistan', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom',
+    'United States', 'Uruguay', 'Uzbekistan, Rep. of', 'Venezuela, Rep. Bolivariana de', 'Vietnam', 'West Bank and Gaza',
+    'Western Sahara', 'Yemen, Rep. of', 'Zambia', 'Zimbabwe'
+]
+
+southern_hemisphere = [
+    'American Samoa', 'Antigua and Barbuda', 'Argentina', 'Aruba, Kingdom of the Netherlands', 'Australia', 'Botswana',
+    'Brazil', 'Chile', 'Colombia', 'Cook Islands', 'Ecuador', 'Falkland Islands (Malvinas)', 'Fiji, Rep. of',
+    'French Polynesia', 'Indonesia', 'Kiribati', 'Madagascar, Rep. of', 'Malawi', 'Marshall Islands, Rep. of the',
+    'Mauritius', 'Mayotte', 'Mozambique, Rep. of', 'Namibia', 'Nauru, Rep. of', 'New Caledonia', 'New Zealand', 'Niue',
+    'Norfolk Island', 'Papua New Guinea', 'Paraguay', 'Peru', 'Pitcairn Islands', 'Samoa', 'São Tomé and Príncipe, Dem. Rep. of',
+    'Solomon Islands', 'South Africa', 'South Sudan, Rep. of', 'St. Helena', 'St. Pierre and Miquelon', 'Suriname',
+    'Tokelau', 'Tonga', 'Tuvalu', 'Uruguay', 'Vanuatu', 'Wallis and Futuna Islands'
+]
 data['Hemisphere'] = data['Country'].apply(lambda x: 'Northern' if x in northern_hemisphere else ('Southern' if x in southern_hemisphere else 'Other'))
 mean_temp_change_north = data[data['Hemisphere'] == 'Northern'].loc[:, '1961':'2022'].mean()
 mean_temp_change_south = data[data['Hemisphere'] == 'Southern'].loc[:, '1961':'2022'].mean()
@@ -114,14 +144,14 @@ fig3.add_trace(go.Scatter(
     y=mean_temp_change_north.values,
     mode='lines+markers',
     name='Northern Hemisphere',
-    line=dict(color='blue')
+    line=dict(color='royalblue')
 ))
 fig3.add_trace(go.Scatter(
     x=mean_temp_change_south.index,
     y=mean_temp_change_south.values,
     mode='lines+markers',
     name='Southern Hemisphere',
-    line=dict(color='red')
+    line=dict(color='tomato')
 ))
 fig3.update_layout(
     title='Temperature Trends in Northern vs Southern Hemisphere (1961-2022)',
@@ -174,21 +204,21 @@ fig5.add_trace(go.Scatter(
     y=temperature_change,
     mode='lines+markers',
     name='Observed',
-    line=dict(color='blue')
+    line=dict(color='royalblue')
 ))
 fig5.add_trace(go.Scatter(
     x=years.flatten(),
     y=temperature_change_pred_linear,
     mode='lines',
     name='Linear Fit',
-    line=dict(dash='dash', color='red')
+    line=dict(dash='dash', color='tomato')
 ))
 fig5.add_trace(go.Scatter(
     x=years.flatten(),
     y=temperature_change_pred_quad,
     mode='lines',
     name='Quadratic Fit',
-    line=dict(dash='dot', color='green')
+    line=dict(dash='dot', color='yellowgreen')
 ))
 fig5.update_layout(
     title='Observed vs Predicted Temperature Change (1961-2022)',
@@ -211,14 +241,14 @@ fig6.add_trace(go.Scatter(
     y=temperature_change,
     mode='lines+markers',
     name='Observed',
-    line=dict(color='blue')
+    line=dict(color='royalblue')
 ))
 fig6.add_trace(go.Scatter(
     x=np.append(years.flatten(), forecast_years),
     y=np.append(temperature_change, forecast),
     mode='lines',
     name='Forecast',
-    line=dict(dash='dash', color='red')
+    line=dict(dash='dash', color='tomato')
 ))
 fig6.update_layout(
     title='Temperature Change Forecast (1961-2032)',
