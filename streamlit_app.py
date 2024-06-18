4292e97bf74122caf18fcd093ec42129c1a51a44
mauriciovargasc <mauriciovargascolon@gmail.com>
2024-06-17 23:16:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -176,6 +176,7 @@ data_decades = data_decades.groupby(year_to_decade, axis=1).mean()
 rate_of_heating_per_decade = data_decades.diff(axis=1).mean(axis=0)
 rate_of_heating_per_decade_df = rate_of_heating_per_decade.reset_index()
 rate_of_heating_per_decade_df.columns = ['Decade', 'Rate of Heating']
+st.write(f'Average Rate of Heating Per Decade: {average_rate_of_heating_per_decade:.4f} °C')
 fig4 = px.bar(rate_of_heating_per_decade_df, 
               x='Decade', 
               y='Rate of Heating',
@@ -184,7 +185,6 @@ fig4 = px.bar(rate_of_heating_per_decade_df,
               template='plotly_dark',
               color_discrete_sequence=['royalblue'])
 average_rate_of_heating_per_decade = rate_of_heating_per_decade.mean()
-st.write(f'Average Rate of Heating Per Decade: {average_rate_of_heating_per_decade:.4f} °C')
 st.plotly_chart(fig4)
 
 # Section 6: Linear and Quadratic Regression
